b2aabbe13b5fda12b07766917ea31a136d999ff8
ajayns <ajay.ns@outlook.com>
2017-12-10 12:06:53 +0530

    Basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  counter:number;
+  pause:boolean = false;
+  timerActive:boolean = false;
+
+  setTimer = (time:number) => {
+    this.counter = time;
+    this.startTimer();
+  }
+
+  togglePause = () => {
+    this.pause = !this.pause;
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    if(!this.timerActive) {
+      this.timerActive = true;
+      this.timer();
+    }
+  }
+   
+  timer = () => {
+      setTimeout(() => {
+        if(this.counter > 0 && this.pause == false) { 
+          this.counter -= 1;
+          this.timer();
+        }
+        else
+          this.timerActive = false;
+      }, 1000);
+  }
 }
